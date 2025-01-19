43496045877b95a378bc1cc592d56b6885de5d0b
imbjdd <b0bb1e@protonmail.com>
2025-01-19 18:18:49 +0100

    feat: add source code #1

--- utils/contract/check.ts
+++ utils/contract/check.ts
@@ -1,4 +1,7 @@
 import { ethers } from "ethers";
+import * as fs from 'fs';
+import Papa from 'papaparse';
+import * as path from 'path';
 
 interface Result {
   code: string;
@@ -24,6 +27,30 @@ async function isERC20(contractAddress:string, provider:any) {
   }
 }
 
+interface CsvRow {
+  Txhash: string;
+  ContractAddress: string;
+  ContractName: string;
+}
+
+const checkTxhashInCsv = async (searchTxhash: string): Promise<CsvRow | null> => {
+  const filePath =  path.resolve(process.cwd(), 'data/export-verified-contractaddress-opensource-license.csv');
+
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(filePath);
+    Papa.parse(fileStream, {
+      header: true,
+      dynamicTyping: true,
+      complete: (result) => {
+        const rows = result.data as CsvRow[];
+        const foundRow = rows.find((row) => row.ContractAddress === searchTxhash);
+        resolve(foundRow || null);
+      },
+      error: reject,
+    });
+  });
+};
+
 export async function check(contractAddress:string) {
   const provider = new ethers.JsonRpcProvider(process.env.INFURA_KEY);
 
@@ -46,6 +73,9 @@ export async function check(contractAddress:string) {
   } else {
     result.isToken = true
   }
+
+  const foundRow = await checkTxhashInCsv(contractAddress);
+  result.isCodeSourcePublished = foundRow?true:false
 
   return result
 }
